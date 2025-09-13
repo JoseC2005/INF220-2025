dd547c3128d06265aee519e05e3071ca893b4dc2
JoseC2005 <justjc00@gmail.com>
2025-09-13 18:33:45 -0400

    3ra revision para vercel

--- proyectos/UI_ADT_lineales/app/app.py
+++ proyectos/UI_ADT_lineales/app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import re
+import os
 
 app = Flask(__name__)
 
@@ -178,4 +179,5 @@ def operacion_conjunto():
 # 🔹 Punto de entrada
 # -------------------------------
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)

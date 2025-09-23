41ea302adfb26409e6cfc658b0482fd248f34896
JoseC2005 <justjc00@gmail.com>
2025-09-23 14:43:01 -0400

    arreglo de run

--- proyectos/UI_ADT_lineales/app/app.py
+++ proyectos/UI_ADT_lineales/app/app.py
@@ -143,3 +143,6 @@ def operacion_conjunto():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
+else:
+    # Para producción en Vercel
+    app = app
